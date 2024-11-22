73f20f2a8ea0f2205022c16f2d22edbd83da9932
asraful islam <asraful009@gmail.com>
2024-11-22 17:43:49 +0600

    level0

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,27 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
     
     [FormerlySerializedAs("Speed")] 
-    public float speed = 100;
+    public float speed = 1000;
+    [FormerlySerializedAs("Movement Speed")]
+    public float movementSpeed = 20;
     
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _rb.AddForce(speed, 0, 0, ForceMode.VelocityChange);
+        _rb.AddForce(speed, 0, 0);
     }
 
     // Update is called once per frame
     private void Update()
     { 
-        _rb.AddForce(speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        _rb.AddForce(new Vector3(speed * Time.deltaTime, 0, 0));
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rb.AddForce(new Vector3(0, 0, -movementSpeed * Time.deltaTime), ForceMode.VelocityChange);
+        } 
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rb.AddForce(new Vector3(0, 0, movementSpeed * Time.deltaTime), ForceMode.VelocityChange);
+        }
     }
 }
