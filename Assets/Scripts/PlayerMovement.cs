10f320b8f31c22b01fc717db21539e4486778170
asraful islam <asraful009@gmail.com>
2024-11-22 14:49:22 +0600

    i1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody _rb;
+    
+    [FormerlySerializedAs("Speed")] 
+    public float speed = 100;
+    
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _rb.AddForce(speed, 0, 0, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update()
+    { 
+        _rb.AddForce(speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     }
 }
